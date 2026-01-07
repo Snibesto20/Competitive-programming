1516d827593ab45ff4208ff4759833fe9f4d1e5c
Snibesto20 <snipisrblx@gmail.com>
2026-01-07 17:55:37 +0200

    10 - Insertion sort

---
+++ algorithms/sorting-algorithms/insertion_sort.py
@@ -0,0 +1,11 @@
+# Time complexity: O(n^2)
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        el = arr[i]
+        j = i - 1
+        while j >= 0 and el < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = el
+    return arr
